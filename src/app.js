58bf3d7ab58daf11d0f8d5640ed2c6acdbd7e4a4
Jack Sampson <jcps@mac.com>
2015-03-17 03:28:40 -0400

    Working skeleton
    This code now successfully hits the API, pulls down groups, accepts user input, and pulls down messages. 
    
    Up next: use settings to get user ID

--- src/app.js
+++ src/app.js
@@ -6,42 +6,119 @@
 
 var UI = require('ui');
 var Vector2 = require('vector2');
+var splashWindow = new UI.Window();
 
-
-var menu = new UI.Menu({
-  sections: [{
-    title: 'Groups',
-    items: [{
-      title: 'Announcements',
-    }, {
-      title: 'Brotherhood'
-    }]
-  }]
+// Text element to inform user
+var text = new UI.Text({
+  position: new Vector2(0, 0),
+  size: new Vector2(144, 168),
+  text:'Downloading GroupMe data...',
+  font:'GOTHIC_28_BOLD',
+  color:'black',
+  textOverflow:'wrap',
+  textAlign:'center',
+  backgroundColor:'white'
 });
-menu.show();
 
-menu.on('select', function(e) {
-  var chat = new UI.Menu({
-    sections: [{
-      title: e.item.title,
-      items: [{
-        title: 'John Sampson',
-        subtitle: 'foo bar',
-      }, {
-        title: 'Clay Langley',
-        subtitle: 'bar foo',
-      }]
-    }]
-  });
-  
-  chat.on('select', function(e){
-    new UI.Card({
-      title: e.item.title,
-      body: e.item.subtitle,
-      scrollable: true,
-    }).show();
+// Add to splashWindow and show
+splashWindow.add(text);
+splashWindow.show();
+
+var parseFeed = function(data){
+  var groups = [];
+  console.log('raw data: ' + data.contents);
+   
+  for(var i = 0; i < data.response.length; i++) {
+  var title = data.response[i].name;
+  var group_id = data.response[i].group_id;
+    groups.push({
+      title:title,
+      group_id:group_id,
+    });
+  }
+  return groups;
+};
+var parseMessages = function(data){
+  var messages = [];
+  console.log('raw data: ' + data.contents);
+   
+  for(var i = 0; i < data.response.messages.length; i++) {
+  var title = data.response.messages[i].name;
+  var text = data.response.messages[i].text;
+    messages.push({
+      title:title,
+      subtitle:text,
+    });
+  }
+  return messages;
+}
+/**
+  AJAX STUFF
+*/
+var ajax = require('ajax');
+ajax(
+  {
+    url: 'https://api.groupme.com/v3/groups?token=INSERT TOKEN HERE',
+    type: 'json'
+  },
+  function(data, status, request) {
     
-  });
-  
-  chat.show();
-});
+    var menuItems = parseFeed(data);
+    
+    // Construct Menu to show to user
+    var groupMenu = new UI.Menu({
+      sections: [{
+        title: 'Groups',
+        items: menuItems
+      }]
+    });
+
+    // Show the Menu, hide the splash
+    groupMenu.show();
+    splashWindow.hide();
+    
+    groupMenu.on('select', function(e) {
+      var ajax2 = require('ajax');
+      console.log('user slelected: ' + e.item.group_id);
+      ajax2(
+        {
+          url: 'https://api.groupme.com/v3/groups/' + e.item.group_id + '/messages?token=INSERT TOKEN HERE',
+          type: 'json'
+        },
+        function(data, status, request){
+          console.log('The second ajax request');
+          var messages = parseMessages(data);
+          console.log('user selected: ' + e.item.title);
+          for(var i = 0; i < messages.length; i++) {
+            console.log(messages[i].title + ' | ' + messages[i].subtitle);
+          }
+          
+          var chat = new UI.Menu({
+            sections: [{
+              title: e.item.title,
+              items: messages,
+            }]
+          });
+          chat.show();
+          
+          chat.on('select', function(e){
+            new UI.Card({
+              title: e.item.title,
+              body: e.item.subtitle,
+              scrollable: true,
+            }).show();  
+          });
+        },
+        function(error, status, request) {
+          console.log('The second ajax request failed: ' + error);
+        }
+      );
+    });
+    
+  },
+  function(error, status, request) {
+    console.log('The ajax request failed: ' + error);
+  }
+);
+
+
